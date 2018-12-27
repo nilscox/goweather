f50a9a5b9fbc07f668d54b6446d6f26e4a9933c9
Nils Layet <nils@epita.fr>
2018-12-27 14:03:10 +0100

    start home page implementation

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'open-iconic/font/css/open-iconic-bootstrap.min.css';
+
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement,
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,27 @@
 import * as React from 'react';
+import { Input, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
 
-const Home = () => (
-  <div>Home</div>
-);
+class Home extends React.Component {
+
+  public render() {
+    return (
+      <div className="p-5">
+
+        <InputGroup>
+          <Input placeholder="Search..." />
+          <InputGroupAddon addonType="append">
+            <InputGroupText>
+              <span className="oi oi-magnifying-glass" />
+            </InputGroupText>
+          </InputGroupAddon>
+        </InputGroup>
+
+        <p className="my-4">Search for a city in the input above.</p>
+
+      </div>
+    );
+  }
+
+}
 
 export default Home;
